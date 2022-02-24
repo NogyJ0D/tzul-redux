e41091ad07a2acac942ef60a6957f20522dbc7c5
NogyJ0D <valentingiarra2@gmail.com>
2022-02-23 23:01:49 -0300

    23-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { login, logout } from './features/user/userSlice'
+
 const App = () => {
+  // Consultar eel estado global
+  const state = useSelector(state => state.user)
+
+  // Actualizar el estado global
+  const dispatch = useDispatch()
+
   return (
-    <div className='app'>
-      App.jsx
+    <div className='app flex flex-col w-max border-2 border-black m-4 p-4'>
+      <p>Usuario: {state.name ? state.name : 'Vacío'}</p>
+      <p>Sesión: {state.logged ? 'Conectado' : 'Desconectado'}</p>
+
+      <button
+        className='border-2 border-black py-1 px-2'
+        onClick={() => {
+          state.logged
+            ? dispatch(logout())
+            : dispatch(login('Nogy'))
+        }}
+      >
+        {state.logged ? 'Salir' : 'Entrar'}
+      </button>
     </div>
   )
 }
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -3,14 +3,17 @@ import { createSlice } from '@reduxjs/toolkit'
 const userSlice = createSlice({
   name: 'user',
   initialState: {
-    logged: false
+    logged: false,
+    name: undefined
   },
   reducers: {
     login (state, action) {
       state.logged = true
+      state.name = action.payload
     },
     logout (state, action) {
       state.logged = false
+      state.name = undefined
     }
   }
 })
